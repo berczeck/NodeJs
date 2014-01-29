c2fa763a44ed3d872bcd2d4843eefe3e0036e737
berczeck_400@hotmail.com <berczeck69@gmail.com>
2014-01-28 21:45:23 -0500

    se aplico estilo al textarea

--- chat-demo01/server.js
+++ chat-demo01/server.js
@@ -1,27 +1,34 @@
-var express = require("express"),
-	app = express(),
-    cons = require('consolidate');
+var express = require("express"),	
+	swig    = require('swig'),
+    cons 	= require('consolidate');
 
+var env = "dev";
+
+var app 	= express(),
+	server 	= require('http').createServer(app); 
+
+swig.init({
+	cache : false
+});
+
+// View engine
 app.engine('html', cons.swig);
 app.set("view engine", "html");
-app.set("views", __dirname + "/views");
+app.set("views", "./app/views");
 
+// Add POST, PUT, DELETE methods to the app
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.methodOverride());
+
+// Static files
+app.use( express.static('./public') );
+
+// Routes
 app.get("/",function(req,res){
-	res.send("Hello World!!! With Node Js",200);
-});
-
-app.get("/luis",function(req,res){
-	res.send("Hello World!!! With Node Js Luis",200);
-});
-
-app.get("/alexander",function(req,res){
-	res.send("Probando mi primer get",200);
-});
-
-app.get("/chat",function(req,res){
 	res.render("chat",{mensaje:"Bienvenido al chat"});
 });
 
-app.listen(8090);
+server.listen(8090);
 
 console.log("Express server started on port 8090")
